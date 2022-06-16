d2122709ea44081cee69812b9ab2467a7bce2119
Yahya Asadi Sheikhi <yahya.asadi.sh@gmail.com>
2022-06-16 23:34:01 +0430

    First commirt

---
+++ index.js
@@ -0,0 +1,93 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+const alphanumeral = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const alphaSymbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let firstPassword = document.getElementById("first-password")
+let secondPassword = document.getElementById("second-password")
+numbersEl = document.getElementById("numbers-el")
+symbolsEl = document.getElementById("symbols-el")
+let length = document.getElementById("length")
+let indexLength = 0
+
+function start(){
+	if (numbersEl.checked && symbolsEl.checked === false) {
+		firstPassword.textContent = generatePasswordAlphanumeral()
+		secondPassword.textContent = generatePasswordAlphanumeral()
+	}
+	else if (numbersEl.checked && symbolsEl.checked) {
+		firstPassword.textContent = generatePasswordAll()
+		secondPassword.textContent = generatePasswordAll()
+	}
+	else if (symbolsEl.checked && numbersEl.checked === false) {
+		firstPassword.textContent = generatePasswordAlphaSymbols()
+		secondPassword.textContent = generatePasswordAlphaSymbols()
+	}else {
+		firstPassword.textContent = generatePasswordAlphabet()
+		secondPassword.textContent = generatePasswordAlphabet()
+	}
+}
+
+function generatePasswordAll() {
+	if (length.value === "") {
+		indexLength = 15
+	}
+	else {
+		indexLength = length.value
+	}
+
+	let password = ""
+	for (let i = 0; i < indexLength; i++){
+		password += characters[Math.floor(Math.random() * characters.length)]
+	}
+	return password
+}
+
+function generatePasswordAlphanumeral() {
+	if (length.value === "") {
+		indexLength = 15
+	}
+	else {
+		indexLength = length.value
+	}
+
+	let password = ""
+	for (let i = 0; i < indexLength; i++){
+		password += alphanumeral[Math.floor(Math.random() * alphanumeral.length)]
+	}
+	return password
+}
+
+function generatePasswordAlphabet() {
+	if (length.value === "") {
+		indexLength = 15
+	}
+	else {
+		indexLength = length.value
+	}
+
+	let password = ""
+	for (let i = 0; i < indexLength; i++){
+		password += alphabet[Math.floor(Math.random() * alphabet.length)]
+	}
+	return password
+}
+
+function generatePasswordAlphaSymbols() {
+	if (length.value === "") {
+		indexLength = 15
+	}
+	else {
+		indexLength = length.value
+	}
+
+	let password = ""
+	for (let i = 0; i < indexLength; i++){
+		password += alphaSymbols[Math.floor(Math.random() * alphaSymbols.length)]
+	}
+	return password
+}
